506073b52a6e1bda6b3537caf17df1c0e2a8be70
Yetife <tifetreats21@gmail.com>
2022-10-07 21:46:35 +0100

    post app files

---
+++ components/PostItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post">
+    <a class="post-header post-header-link clickable">
+      <h4 class="title is-4">{{title}}</h4>
+      <h5 class="subtitle is-5">{{ subtitle }}</h5>
+    </a>
+    <div class="post-content">
+      by Filip Jerga, 27th Jan, 2019
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: "PostItem",
+  props:{
+    title:{
+      type: String,
+      required:true
+    },
+    subtitle:{
+      type: String,
+      required: true
+    },
+    date:{
+      type: Date,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'post-app',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://cdn.jsdelivr.net/npm/marked/marked.min.js'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
